7ee8e1af619f9b98f30781173c5e565f61f7a63f
Anna <kochurovaav@gmail.com>
2023-04-12 15:29:52 +0300

    Sorting1 is added

---
+++ Bootcamp13/Sorting1/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] array = {0, 2, 3, 3, 1, 5, 9, 1, 1};
+CountingSort(array);
+Console.WriteLine(string.Join(",", array));
+
+void CountingSort (int[] inputArray)
+{
+    int [] counters = new int [10];
+       //  int ourNumbers;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+     
+        // int ourNumbers = inputArray[i];
+        // counters[ourNumbers] ++;
+    }
+
+int index = 0;
+for(int i = 0; i < counters.Length; i++)
+{
+    for(int j = 0; j < counters[i]; j++)
+    {
+        inputArray [index] = i;
+        index++;
+    }
+}
+
+}
